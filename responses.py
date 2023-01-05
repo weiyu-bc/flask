1d0820b9273016bb8e36360a38bef633b2609fb1
Alex <110878066+NWFT@users.noreply.github.com>
2023-01-05 15:08:38 -0800

    Update responses.py

--- responses.py
+++ responses.py
@@ -8,27 +8,27 @@ def handle_response(msg):
         return 'Hello!'
 
     if p_msg.startswith('?id'):
-        return '30800001'
+        return ''
 
     if p_msg.startswith('process 1'):
         return str(random.randint(1, 6))
 
     if p_msg.startswith('?p1'):
-        return "您的福利币配额为10000HCN。"
+        return "。"
 
     if p_msg.startswith('?join'):
-        return "您加入农场时间：2022年06月04日"
+        return ""
 
     if p_msg.startswith('?9'):
-        return "Discord: 明心#777, 点击进入连接."
+        return "."
 
     if p_msg.startswith('?help'):
         info = """
         ?help  帮助信息；
-        ?id     查询红叶农场会员信息；
-        ?p1  查询喜币配额；
-        ?join   查询加入农场时间；
-        ?9  联系人工客服；
+        ?id     ；
+        ?p1  查询；
+        ?join   查询；
+        ?9  联系；
         """
         return info
 
